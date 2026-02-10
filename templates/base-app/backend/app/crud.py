eeaf57c6cadd40689ecae6da121261c6e9cc5ce5
Ruchira S. Datta <ruchira@dattascience.com>
2026-02-10 12:05:43 -0800

    Added create_user().

--- templates/base-app/backend/app/crud.py
+++ templates/base-app/backend/app/crud.py
@@ -3,6 +3,15 @@ from app.models import User
 from app.database import SessionDep
 
 
+def create_user(id: int, email: str, fullname: str = None, 
+    orcid: str = None, is_admin: bool = False, profile_url: str = None):
+    db_user = User(id=user_id, email=email, fullname=fullname, orcid=orcid,
+                    is_admin=is_admin, profile_url=profile_url)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return(db_user)
+    
 def get_user_by_id(db: SessionDep, user_id: int) -> User | None:
     user = db.scalar(select(User).filter_by(id=user_id))
     return user
